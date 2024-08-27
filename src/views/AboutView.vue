a3ce01af8f713d7fefd4bafe744a47ecd6b34e0e
young <55731744@qq.com>
2024-08-27 22:10:51 +0800

    add tmp

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,38 @@
+
+<script lang="ts" setup>
+import { Button } from 'vant';
+import { Image as VanImage } from 'vant';
+import logo from '@/assets/logo.svg'
+import {onMounted} from "vue";
+
+function Delay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('执行完毕')
+    }, 2000)
+  })
+}
+async function foo() {
+  // console.log('foo')
+  Promise.resolve('foo').then(res => {
+    console.log(res);
+  })
+}
+onMounted( async () => {
+  foo()
+})
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-image :src="logo"></van-image>
+    <Button type="primary">click</Button>
   </div>
 </template>
-
-<style>
+<style lang="less" scoped>
+h1 {
+  font-size: 36px;
+}
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
