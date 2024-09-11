f82ed43207832c6eaa1e7b62230910c0aac60317
young <55731744@qq.com>
2024-09-11 17:59:17 +0800

    添加require 兼容的插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import ptw from 'postcss-px-to-viewport'
+import vitePluginRequire from 'vite-plugin-require'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    vitePluginRequire.default()
   ],
   resolve: {
     alias: {
