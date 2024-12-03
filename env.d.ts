7dad5e56d7594095f9f3eafc1a650c1f145be964
young <55731744@qq.com>
2024-12-03 14:00:33 +0800

    添加vite.config相关配置

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,16 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-env" />
+
 declare module "*.vue" {
     import { DefineComponent } from "vue"
     const component: DefineComponent<{}, {}, any>
     export default component
+}
+interface ImportMetaEnv {
+    VITE_APP_TITLE: string;
+    // more environment variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,27 @@ import vitePluginRequire from 'vite-plugin-require'
 export default defineConfig({
   plugins: [
     vue(),
+    // 兼容jsx语法
     vueJsx(),
-    vitePluginRequire.default()
+    // 兼容require方法写法
+    (vitePluginRequire as any).default(),
+    // if necessary
+    // legacy({
+    //   // 是否渲染现代浏览器模块，关闭可一定程度提高打包速度，开启后会打包两套代码
+    //   renderModernChunks: true,
+    //   // 手动添加更新颗粒度的相关polyfills
+    //   // polyfills: [],
+    //   // 如果开发者希望添加非corejs的polyfill，就加在这个字段里面
+    //   additionalLegacyPolyfills: [],
+    // })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    // 默认modules，es2015选项可以对语法进行降级es6
+    target: 'es2015',
   }
 })
