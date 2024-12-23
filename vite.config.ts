15254419646cb6d61f8ef13f19dbc03dcd3c662e
young <55731744@qq.com>
2024-12-23 10:27:15 +0800

    插件配置，重写

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import {fileURLToPath, URL} from 'node:url'
 
-import {defineConfig} from 'vite'
+import {defineConfig, type PluginOption} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vitePluginRequire from 'vite-plugin-require'
 import legacy from '@vitejs/plugin-legacy'
 import ptw from 'postcss-px-to-viewport'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        // 兼容jsx语法
-        vueJsx(),
-        // 兼容require方法写法
-        (vitePluginRequire as any).default(),
-        // if necessary
+const needLegacy: boolean = false
+const plugins: PluginOption [] = [
+    vue(),
+    // 兼容jsx语法
+    vueJsx(),
+    // 兼容require方法写法
+    (vitePluginRequire as any).default(),
+]
+if (needLegacy) {
+    plugins.push(
         legacy({
             // 是否渲染现代浏览器模块，关闭可一定程度提高打包速度，开启后会打包两套代码
             // renderModernChunks: true,
@@ -30,7 +31,11 @@ export default defineConfig({
                 'Android>=5',
             ],
         })
-    ],
+    )
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins,
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -53,7 +58,7 @@ export default defineConfig({
     },
     build: {
         // 默认modules，es2015选项可以对语法进行降级es6
-        // target: 'es2015',
+        target: 'es2015',
         rollupOptions: {
             output: {
                 manualChunks(id) {
