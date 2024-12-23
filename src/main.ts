664f3e3426e10b24587337bb1a40d48d514e9477
young <55731744@qq.com>
2024-12-23 11:19:24 +0800

    引入 vconsole。并且动态引入

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,16 @@ import App from './App.vue'
 import router from './router'
 import 'vant/lib/index.css';
 import {useVant} from "@/hooks/useVant";
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 useVant(app)
 app.mount('#app')
+if (import.meta.env.PROD) {
+    import('vconsole').then(vconsole => {
+        new vconsole.default({
+            theme: 'dark',
+        })
+    })
+}
